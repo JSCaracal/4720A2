7eac1678a2700c3d4689129d8bc0ba471b88612f
JSCaracal <siposj@knights.ucf.edu>
2023-08-29 15:13:25 -0400

    Adds assigment files

---
+++ main.py
@@ -0,0 +1,46 @@
+# Import necessary libraries
+import pygame as pg
+from OpenGL.GL import *
+import numpy as np
+
+# Initialize pygame
+pg.init()
+
+# Set up OpenGL context version
+pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
+pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
+
+# Create a window for graphics using OpenGL
+width = 640
+height = 480
+pg.display.set_mode((width, height), pg.OPENGL | pg.DOUBLEBUF)
+
+# Todo: Part 1
+# Set the background color (clear color)
+# glClearColor takes 4 arguments: red, green, blue, alpha. Each argument is a float between 0 and 1.
+glClearColor(0.3, 0.4, 0.5, 1.0)
+
+
+# Todo: Part 2
+
+# Todo: Part 3
+
+# Todo: Part 4
+
+
+# Run a loop to keep the program running
+draw = True
+while draw:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            draw = False
+
+    # Clear the screen (or clear the color buffer), and set it to the background color chosen earlier
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    # Refresh the display to show what's been drawn
+    pg.display.flip()
+
+
+pg.quit()   # Close the graphics window
+quit()      # Exit the program
